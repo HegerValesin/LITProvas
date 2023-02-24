ac9b41bddd51951064765b14adb14fa3217f98d7
Heger Padilha <hegervfp@gmail.com>
2023-02-24 18:10:27 -0300

    ajuste da tela de questão e sequencias

--- src/telas/provas/script.js
+++ src/telas/provas/script.js
@@ -4,13 +4,17 @@ let questions = [];
 // Índice da pergunta atual
 let currentQuestionIndex = 0;
 
+// Criando uma array para armazenar as respostas do usuário
+const userAnswers = [];
+
 // Elementos do HTML que serão atualizados
-const questionTextElement = document.getElementById('question-text');
+const questionTextElement = document.getElementById('qtext');
 const answerOptionsElement = document.getElementById('answer-options');
-const nextButton = document.getElementById('next-button');
+const nextButton = document.getElementById('nextbutton');
 const currentQuestionNumberElement = document.getElementById('current-question');
 const answeredStatusElement = document.getElementById('answered-status');
 const currentPointsElement = document.getElementById('current-points');
+const previousButton = document.getElementById("previousbutton");
 
 // Carrega as perguntas do arquivo exam.json
 fetch('../../../public/exam.json')
@@ -20,6 +24,7 @@ fetch('../../../public/exam.json')
 
         console.log(questions);
         showCurrentQuestion();
+        renderQuestion();
     });
 
 // Mostra a pergunta atual
@@ -27,78 +32,98 @@ function showCurrentQuestion() {
     // Obter a pergunta atual
     const currentQuestion = questions[currentQuestionIndex];
 
-    console.log(currentQuestion);
+    
     // Renderiza o título da questão
-    const questionTitle = document.querySelector('.qtext');
-    questionTitle.innerHTML = `${currentQuestion.statement}`;
+    questionTextElement.innerHTML = `${currentQuestion.statement}`;
 
     // Renderiza as opções de resposta
-    const answerOptions = document.querySelector('.answer');
-    answerOptions.innerHTML = '';
-    if (currentQuestion.type === 0) {
-        // Questão subjetiva (text)
-        answerOptions.innerHTML = `<textarea id="answer" class="subjetiva"></textarea>`;
-    } else if (currentQuestion.type === 1) {
-        // Questão de múltipla escolha
-        currentQuestion.alternatives.alternative.forEach((answer) => {
-            console.log(answer.alternativeText)
-            const newAnswerOption = document.createElement('div');
-            newAnswerOption.classList.add('r' + answer.alternativeOrder % 2);
+    while (answerOptionsElement.firstChild) {
+        answerOptionsElement.removeChild(answerOptionsElement.firstChild);
+    }
+       
+
+    if(currentQuestion.type === 0){
+            answerOptionsElement.innerHTML = `<textarea id="answer" class="subjetiva" rows="15" cols="60"></textarea>`;
+        }else if (currentQuestion.type === 1) {
+            
+            for (let i = 0; i < currentQuestion.alternatives.alternative.length; i++) {
+             const choice = currentQuestion.alternatives.alternative[i];
+             const newAnswerOption = document.createElement('div');
+             newAnswerOption.classList.add('r' + choice.alternativeOrder % 2);
 
             newAnswerOption.innerHTML = `
-      <input type="radio" name="answer" id="${answer.alternativeOrder}" value="${answer.alternativeOrder}" />
-      <label for="${answer.alternativeOrder}">${answer.alternativeText}</label>
-    `;
-
-            answerOptions.appendChild(newAnswerOption);
-        });
+                <input type="radio" name="answer" id="${choice.alternativeOrder}" value="${choice.alternativeOrder}" />
+                <label for="${choice.alternativeOrder}">${choice.alternativeText}</label>
+            `;
+            answerOptionsElement.appendChild(newAnswerOption);
+            }
     }
 
-    // Adiciona o evento de click no botão "Próximo"
-    const nextButton = document.querySelector('#next-button');
-    nextButton.addEventListener('click', () => {
-        // Verifica se a questão foi respondida corretamente
-        const answer = getAnswer();
-        if (answer === null) {
-            alert('Por favor, responda a questão antes de prosseguir.');
-            return;
+} 
+    // Adiciona o evento de click no botão "Voltar"
+    const prevButton = document.querySelector('#previousbutton');
+    prevButton.addEventListener("click", () => {
+        currentQuestionIndex--;
+        showCurrentQuestion();
+        showPrevisiousQuestion();
+        if (currentQuestionIndex === 0) {
+            prevButton.disabled = true;
         }
-
-        // Armazena a resposta
-        storeAnswer(answer);
-
-        // Passa para a próxima questão ou finaliza o questionário
-        currentQuestionIndex++;
-        if (currentQuestionIndex < questions.length) {
-            renderQuestion();
-        } else {
-            finishQuiz();
-        }
+        nextButton.disabled = false;
     });
 
-
-}
-
-
-function getAnswer() {
-    const currentQuestion = questions[currentQuestionIndex];
-    if (currentQuestion.type === 0) {
-        // Questão subjetiva
-        console.log("click01")
-        const answerTextarea = document.querySelector('#answer');
-        return answerTextarea.value.trim() !== '' ? answerTextarea.value : null;
-    } else if (currentQuestion.type === 1) {
-        // Questão de múltipla escolha
-        console.log("click02")
-        const answerRadioButtons = document.querySelectorAll('input[type=radio]');
-        for (let i = 0; i < answerRadioButtons.length; i++) {
-            if (answerRadioButtons[i].checked) {
-                return answerRadioButtons[i].value;
-            }
+    // Adiciona o evento de click no botão "Proximo"
+    nextButton.addEventListener("click", function() {
+        currentQuestionIndex++;
+        showCurrentQuestion()
+        renderQuestion()
+        if (currentQuestionIndex === questions.length - 1) {
+            nextButton.disabled = true;
         }
-        return null;
+        prevButton.disabled = false;
+    });
+    
+
+
+    function renderQuestion() {
+        // Atualiza o número da pergunta atual
+        currentQuestionNumberElement.innerText = `${currentQuestionIndex + 1}`;
+    
+        // Atualiza o status da pergunta (respondida ou não)
+        const currentQuestion = questions[currentQuestionIndex];
+        const isAnswered = null;
+        answeredStatusElement.innerText = isAnswered ? 'Respondida' : 'Ainda não Respondida';
+    
+        // Atualiza a pontuação atual (se houver)
+        if (currentQuestion.points !== undefined) {
+            currentPointsElement.innerText = `${currentQuestion.points}`;
+        } else {
+            currentPointsElement.innerText = '';
+        }
+        
     }
-}
+
+    function showPrevisiousQuestion() {
+        let correnteQestionInd = parseFloat(document.getElementById('current-question').innerText);
+        
+        currentQuestionNumberElement.innerText = `${correnteQestionInd - 1}`;
+        
+         // Atualiza o status da pergunta (respondida ou não)
+         const currentQuestion = questions[currentQuestionIndex];
+         const isAnswered = 1;
+         answeredStatusElement.innerText = isAnswered ? 'Respondida' : 'Ainda não Respondida';
+          // Atualiza a pontuação atual (se houver)
+         if (currentQuestion.points !== undefined) {
+             currentPointsElement.innerText = `${currentQuestion.points}`;
+         } else {
+             currentPointsElement.innerText = '';
+         }
+    }
+
+    console.log(currentQuestionIndex)
+    ////////////////////////////////////////////////////////
+ /*
+  
 
 function storeAnswer(answer) {
     // Armazena a resposta em algum lugar (por exemplo, em um objeto ou array)
@@ -126,7 +151,24 @@ function renderQuestion() {
     } else {
         currentPointsElement.innerText = '';
     }
-
     // Renderiza a pergunta atual
     showCurrentQuestion();
 }
+
+function showPrevisiousQuestion() {
+    currentQuestionNumberElement.innerText = `${currentQuestionIndex--}`;
+    
+     // Atualiza o status da pergunta (respondida ou não)
+     const currentQuestion = questions[currentQuestionIndex];
+     const isAnswered = getAnswer() !== null;
+     answeredStatusElement.innerText = isAnswered ? 'Ainda não Respondida' : 'Respondida';
+      // Atualiza a pontuação atual (se houver)
+     if (currentQuestion.points !== undefined) {
+         currentPointsElement.innerText = `${currentQuestion.points}`;
+     } else {
+         currentPointsElement.innerText = '';
+     }
+      // Renderiza a pergunta atual
+     showCurrentQuestion();
+}
+*/
